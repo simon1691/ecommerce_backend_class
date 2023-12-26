e527b99b7097e970169f7cbd291930fc8ada935b
simon <simonagudelo93@gmail.com>
2023-12-26 17:43:51 -0500

    12-26-23 add and remove products functionality added

--- src/public/js/cart.js
+++ src/public/js/cart.js
@@ -2,30 +2,60 @@ const extractCookie = (cookieName) => {
     return document.cookie.split(';').find((item) => item = item.trim().startsWith(`${cookieName}`)).split('=')[1]
 }
 
-const addButton = document.getElementById('more-product')
-const sustractButton = document.getElementById('less-product')
+const addButton = document.getElementsByClassName('more-product')
+const sustractButton = document.getElementsByClassName('less-product')
 const quantityInput = document.getElementsByClassName('cart-item-quantity')
-// const productsInCart = JSON.parse(extractCookie('productId')).products
 
 const addQuantity = async (e) => {
     e.preventDefault()
-    const productId = e.target.parentElement.parentElement.parentElement.parentElement.id.split('-')[1]
+    const productId = e.target.id.split('-')[2]
     const quantityValue = e.target.value
-    
-    if (quantityValue <= 0 || quantityValue === ' ' || quantityValue === null) return
+    const productTotalId = document.getElementById(`cart-total-${productId}`)
+    const priceUnitId = document.getElementById(`price-unit-${productId}`)
+    const priceUnitValue = parseFloat(priceUnitId.innerHTML)
+
+    if (quantityValue <= 0 || quantityValue === ' ' || quantityValue === null || quantityValue === NaN) return
 
     const response = await fetch(`/api/carts/${cartId}/product/${productId}`, {
         method: 'PUT',
-        body: JSON.stringify({quantity: quantityValue}),
+        body: JSON.stringify({quantity: parseFloat(quantityValue)}),
         headers: {
             'Content-Type': 'application/json'
         }
     })
+    productTotalId.innerHTML = priceUnitValue * parseFloat(quantityValue)
+
     const data = await response.json()
     console.log(data)
 }
 
-Array.from(quantityInput).forEach((input) => input.addEventListener('input', (e) => addQuantity(e)))
+// adds the quantity of the product by one to the cart
+const addQuantityByOne = (e) => {
+    e.preventDefault()
+    const inputId = document.getElementById(e.target.previousElementSibling.id)
+    inputId.value = parseFloat(inputId.value) + 1
+    inputId.dispatchEvent(new Event('input'))
+}
 
-// quantityInput.addEventListener('input', (e) => addQuantity(e))
+const removeQuantityByOne = (e) => {
+    e.preventDefault()
+    const inputId = document.getElementById(e.target.nextElementSibling.id)
+    if(inputId.value <= 1) return
+    inputId.value = parseFloat(inputId.value) - 1
+    inputId.dispatchEvent(new Event('input'))
+
+}
+// converts de quantityInput variable to and array and then adds it the event listener to perform the addition of the quantity
+let inputsArray = Array.from(quantityInput)
+inputsArray.forEach((input) => input.addEventListener('input', (e) => addQuantity(e)))
+
+let moreProductArray = Array.from(addButton)
+moreProductArray.forEach((button) => button.addEventListener('click', (e) => addQuantityByOne(e)))
+
+console.log(moreProductArray)
+let lessProductArray = Array.from(sustractButton)
+lessProductArray.forEach((button) => button.addEventListener('click', (e) => removeQuantityByOne(e)))
+
+
+
 
